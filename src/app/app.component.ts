cb20fd0ba32e663426ad7c9b94715abb661e4d9d
amusa4 <itsadilmusa@gmail.com>
2020-07-05 11:27:17 +0100

    Notes setup and standard changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Note } from './note';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
+
   title = 'notes-app';
+  firstNote = new Note('First Note', 'HEYYY');
+  Notes = [this.firstNote];
+
 }
---
+++ src/app/note.ts
@@ -0,0 +1,13 @@
+export class Note
+{
+  title: string;
+  body: string;
+  constructor(title, body)
+  {
+    this.title = title;
+    this.body = body;
+  }
+
+}
+
+
